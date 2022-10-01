d4736dfb60e0e54239b3f3ab7a17ea010ee927e9
Hetav <hetavshah1705@gmail.com>
2022-10-01 23:23:55 +0530

    Commented Recruiter Route

--- routers/recruiterRouter.js
+++ routers/recruiterRouter.js
@@ -12,26 +12,26 @@ recruiterRouter
 .post(login);
 
 recruiterRouter
-.route('/:id')
-.get(protectRoute,getRecruiterById)
-.patch(protectRoute,updateRecruiterById)
-.delete(protectRoute,deleteRecruiterById);
+// .route('/:id')
+// .get(protectRoute,getRecruiterById)
+// .patch(protectRoute,updateRecruiterById)
+// .delete(protectRoute,deleteRecruiterById);
 
-recruiterRouter
-.route('/:id/job')
-.get(protectRoute,jobPostedByRecId)         // Details of all the jobs posted By Recruiter 
-.post(protectRoute,createJob);
+// recruiterRouter
+// .route('/:id/job')
+// .get(protectRoute,jobPostedByRecId)         // Details of all the jobs posted By Recruiter 
+// .post(protectRoute,createJob);
 
-recruiterRouter
-.route('/:id/job/:jobid')
-.patch(protectRoute,updateJobDetails)
-.delete(protectRoute,deleteJobById)
+// recruiterRouter
+// .route('/:id/job/:jobid')
+// .patch(protectRoute,updateJobDetails)
+// .delete(protectRoute,deleteJobById)
 
-recruiterRouter
-.route('/:id/job/:jobid/response')
-.get(protectRoute,getAllCandidateDetails)
-.post(protectRoute,candidateSelection)
-.patch(protectRoute,updateSelection)
+// recruiterRouter
+// .route('/:id/job/:jobid/response')
+// .get(protectRoute,getAllCandidateDetails)
+// .post(protectRoute,candidateSelection)
+// .patch(protectRoute,updateSelection)
 
 
 
