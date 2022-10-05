b6f5b73e7a9324dbd44704756bc424dfec088c0e
HetavShah <96936720+HetavShah@users.noreply.github.com>
2022-10-05 08:41:57 +0530

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    JWT_KEY:'zsfhrtw3b8c22j0i4quoh09y2r2mlgsr',
-    DB_LINK:'postgres://hapbdmvaeqicms:934c2288979db9191815178af8fef4676123efecad8538e91d742a67d4bf422d@ec2-54-209-66-211.compute-1.amazonaws.com:5432/d1kqvdl0ca6ojj'
-};
