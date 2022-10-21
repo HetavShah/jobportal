352aef280e13b91e0391e062983c19963091bc00
Hetav <hetavshah1705@gmail.com>
2022-10-21 09:56:56 +0530

    security fix

--- config/database.js
+++ config/database.js
@@ -11,16 +11,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
          },
          logging:false
 })
-// const sequelize = new Sequelize('d1kqvdl0ca6ojj', 'hapbdmvaeqicms', '934c2288979db9191815178af8fef4676123efecad8538e91d742a67d4bf422d', {
-//     host: 'ec2-54-209-66-211.compute-1.amazonaws.com',
-//     dialect: 'postgres',/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-//     logging:false,
-//     "dialectOptions": {
-//       "ssl": {
-//         "require": true,
-//         "rejectUnauthorized": false
-//       }
-//     }
-//   });
+
   
 module.exports = sequelize;
