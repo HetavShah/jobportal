574ee45d3c62fa139fb7dfed59297b17bc4600f0
HetavShah <hetavshah1705@outlook.com>
2023-05-22 09:46:09 +0530

    Configure for remote cluster

--- config/database.js
+++ config/database.js
@@ -1,9 +1,28 @@
 const Sequelize=require('sequelize');
 require('dotenv').config();
 const DB_LINK=process.env.DATABASE_URL;
-const sequelize = new Sequelize(DB_LINK,{
-         logging:false
-})
+let sequelize;
+if(DB_LINK==process.env.LOCAL_DB_LINK)
+
+{
+  
+   sequelize = new Sequelize(DB_LINK,{
+  
+           logging:false,
+  });
+}
+else{
+  sequelize= new Sequelize(DB_LINK,{
+    logging:false,
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+     }
+     },
+  });
+
+}
 
   
 module.exports = sequelize;
